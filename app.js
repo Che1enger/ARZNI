1f022f9a395b94456ce4b18c479e7299dbc3c4e9
Che1enger <yasprimorki@gmai.com>
2024-09-18 17:56:37 +0300

    fix 1

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 function showCard(card) {
-    if (window.innerWidth > 500) {
+  if (window.innerWidth > 500) {
       var modal = document.getElementById("modal");
       var modalContent = document.getElementById("modal-content");
       var cardClone = card.cloneNode(true);
       var imageBox = cardClone.querySelector(".image-box");
       var img = imageBox.querySelector("img");
-  
-      cardClone.style.transform = "scale(1.3)";
-      img.style.transform = "scale(1.2)"; // Добавленная строка для масштабирования изображения
-  
+
+      cardClone.style.width = "30vw";  
+      cardClone.style.height = "62vh";
+
+      img.style.transform = "scale(1.3)";  
+      imageBox.style.height = "55vh";
+
+    
+      modalContent.style.fontSize = "clamp(1rem, 2vw, 2.5rem)";  // Минимум 1rem, адаптивный 2vw, максимум 2.5re
+
       modalContent.innerHTML = "";
       modalContent.appendChild(cardClone);
       modal.style.display = "flex";
       cardClone.style.boxShadow = '0px 0px 0 0 black';
-    }
   }
+}
+
 
 document.getElementById("modal").addEventListener("click", function(event) {
     if (event.target === this) {
